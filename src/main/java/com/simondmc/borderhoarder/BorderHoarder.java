c804359984f5074451279004b02be02238e073cd
SimonDMC <simondmcplayer@gmail.com>
2022-09-25 10:00:52 +0200

    Initialize repository

---
+++ src/main/java/com/simondmc/borderhoarder/BorderHoarder.java
@@ -0,0 +1,16 @@
+package com.simondmc.borderhoarder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BorderHoarder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
