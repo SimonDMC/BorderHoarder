91d073d0ca35030ef22488636de1c12c915363ca
SimonDMCPlayer <46278840+SimonDMCPlayer@users.noreply.github.com>
2022-09-28 13:43:49 +0200

    Fix inconsistent border sizes

--- src/main/java/com/simondmc/borderhoarder/world/BorderExpander.java
+++ src/main/java/com/simondmc/borderhoarder/world/BorderExpander.java
@@ -1,12 +1,13 @@
 package com.simondmc.borderhoarder.world;
 
 import com.simondmc.borderhoarder.BorderHoarder;
+import com.simondmc.borderhoarder.game.ItemHandler;
 import org.bukkit.World;
 
 public class BorderExpander {
 
     public static void expandBorder() {
         World w = BorderHoarder.plugin.getServer().getWorld(BorderWorldCreator.worldName);
-        w.getWorldBorder().setSize(w.getWorldBorder().getSize() + 2, 1L);
+        w.getWorldBorder().setSize(ItemHandler.getCollectedItems().size() * 2 + 1, 1L);
     }
 }
