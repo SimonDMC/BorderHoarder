fa407505701adc6442514a2de11c4838f9e02e45
SimonDMC <simondmcplayer@gmail.com>
2022-10-01 20:17:10 +0200

    Fix advancements carrying over between games

--- src/main/java/com/simondmc/borderhoarder/util/PlayerUtil.java
+++ src/main/java/com/simondmc/borderhoarder/util/PlayerUtil.java
@@ -1,7 +1,12 @@
 package com.simondmc.borderhoarder.util;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementProgress;
 import org.bukkit.entity.Player;
+
+import java.util.Iterator;
 
 public class PlayerUtil {
     public static void preparePlayer(Player p) {
@@ -18,5 +23,13 @@ public class PlayerUtil {
         p.setFallDistance(0);
         p.setAllowFlight(false);
         p.setFlying(false);
+        // https://www.spigotmc.org/threads/clearing-every-advancement-a-player-has-on-the-server.473875/
+        Iterator<Advancement> iterator = Bukkit.getServer().advancementIterator();
+        while (iterator.hasNext()) {
+            AdvancementProgress progress = p.getAdvancementProgress(iterator.next());
+            for (String criteria : progress.getAwardedCriteria()) {
+                progress.revokeCriteria(criteria);
+            }
+        }
     }
 }
