8d27b22ba0b0d408fdaed48b150863b76a124566
SimonDMC <simondmcplayer@gmail.com>
2022-10-01 19:54:50 +0200

    Fix player scores not displaying properly in tab list

--- src/main/java/com/simondmc/borderhoarder/game/TabHandler.java
+++ src/main/java/com/simondmc/borderhoarder/game/TabHandler.java
@@ -1,12 +1,14 @@
 package com.simondmc.borderhoarder.game;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Criteria;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class TabHandler {
     public static void updateTab() {
@@ -17,7 +19,10 @@ public class TabHandler {
 
         // set player scores
         for (Player p : Bukkit.getOnlinePlayers()) {
-            o.getScore(p).setScore(Collections.frequency(ItemHandler.getCollectedItems().values(), p));
+            // get amount of items collected by UUID
+            o.getScore(p).setScore(Collections.frequency(ItemHandler.getCollectedItems().values().stream().map(
+                    Entity::getUniqueId
+            ).collect(Collectors.toList()), p.getUniqueId()));
         }
     }
 }
